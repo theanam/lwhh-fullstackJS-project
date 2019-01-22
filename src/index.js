fefccff3a71d99f6aa5030592bb656121649aaef
Anam Ahmed <anam.ahmed.a@gmail.com>
2019-01-22 10:47:22 +0600

    authorization middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bp      = require('body-parser');
 const signup  = require('./controllers/signup');
 const login   = require('./controllers/login');
+const auth    = require('./middlewares/auth');
 const app     = express();
 
 // Middlewares
 app.use(bp.json());
-
+app.use('/api',auth);
 //Routes
 app.use(signup);
 app.use(login);
