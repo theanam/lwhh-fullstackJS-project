1e65a3edc6f6f63cab9b95ff3e32ddaa4b12f510
anam.ahmed <anam.ahmed.a@gmail.com>
2019-03-10 04:28:01 +0600

    polymorphic component

--- frontend/src/components/loginsignup.js
+++ frontend/src/components/loginsignup.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react'
 
 export default class LoginSignup extends Component {
+  state={
+      isSignup:false
+  }
+  showSignupForm = ()=>{
+      this.setState({isSignup:true})
+  }
+  showLogin = ()=>{
+    this.setState({isSignup:false})
+  }
   render() {
     return (
       <div style={styles.container}>
         <div className="loginsignup">
-            <input style={styles.input} type="text" name="fullname" placeholder="full name"/>
+            <input style={{...styles.input,display:this.state.isSignup?'block':'none'}} type="text" name="fullname" placeholder="full name"/>
             <input style={styles.input} type="email" name="email" placeholder="Email Address"/>
             <input style={styles.input} type="password" name="password" placeholder="Enter password"/>
         </div>
+        <div style={{display:this.state.isSignup?'none':'block'}} onClick={this.showSignupForm}>Sign Up Now</div>
+        <div style={{display:this.state.isSignup?'block':'none'}} onClick={this.showLogin}>Go Back To Login</div>
       </div>
     )
   }
 }
 
 const styles = {
-    container:{
-        fontSize:24,
-        color:'red'
-    },
     input:{
         fontSize:18,
         color:'blue',
         display:'block',
-        margin:20
+        margin:20,
+        // display:'none'
     }
 }
