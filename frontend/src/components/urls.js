d1e41c25ac03d8ea1c5a0d45721177416a788836
anam.ahmed <anam.ahmed.a@gmail.com>
2019-03-17 04:14:37 +0600

    missing file

---
+++ frontend/src/components/urls.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Urls extends Component {
+  render() {
+    return (
+      <div>
+        private component
+      </div>
+    )
+  }
+}
