f3e09398ff9854ed1b770b22ca4ec0543579cebf
anam.ahmed <anam.ahmed.a@gmail.com>
2019-03-17 04:14:27 +0600

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import LoginSignup from './components/loginsignup';
-
+import Urls from './components/urls';
+import { BrowserRouter as Router, Route, Link,Redirect } from "react-router-dom";
+function Home(){
+  return (<div>
+      <Link to="/loginsignup">Login signup</Link>
+    </div>);
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginSignup/>
+        <Router>
+          <Route exact path="/" component={Home}/>
+          <Route path="/loginsignup" component={LoginSignup}/>
+          <Route path="/urls" component={Urls}/>
+        </Router>
       </div>
     );
   }
