54ecb463f67bffc372355a4447043a6a21caf4f6
anam.ahmed <anam.ahmed.a@gmail.com>
2019-04-02 02:24:23 +0600

    heroku deploy

---
+++ frontend/build/precache-manifest.e28160e6b94cbcfcd22c00570ce16fcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "6e895cd295899d4afc20",
+    "url": "/static/js/main.fd172b32.chunk.js"
+  },
+  {
+    "revision": "9367c7633b4eea282582",
+    "url": "/static/js/2.c24d3e34.chunk.js"
+  },
+  {
+    "revision": "6e895cd295899d4afc20",
+    "url": "/static/css/main.1d2cd0c4.chunk.css"
+  },
+  {
+    "revision": "a28efb052afda539b9036aa22603e9bc",
+    "url": "/index.html"
+  }
+];
--- src/controllers/redirects.js
+++ src/controllers/redirects.js
@@ -3,7 +3,7 @@ const {Direction}    = require('../utils/db');
 const {check}        = require('express-validator/check');
 const rejectInvalid  = require('../middlewares/reject_invalid');
 const _p             = require('../utils/promise_errors');
-
+const path           = require('path');
 const entryValidator = [check('url').isURL()]
 router.post('/api/v1/redirects',entryValidator,rejectInvalid,async (req,res,next)=>{
     let user_id = req.user.id;
@@ -29,11 +29,28 @@ router.get('/api/v1/redirects',async (req,res)=>{
     let [dberr,myDirections] = await _p(Direction.findAll({
         where:{
             'user_id':req.user.id
-        },
-        limit:10
+        }
     }));
     if(dberr) return next(dberr);
     return res.json(myDirections.map(d=>{return {hash:d.hash,destination:d.destination,id:d.id,created_at:d.createdAt}}));
 });
-
+router.get(`/:hash`,async (req,res,next)=>{
+    let URLhash = req.param('hash');
+    let [dberr,hashDirection] = await _p(Direction.find({
+        where:{
+            'hash':URLhash
+        }
+    }));
+    if(dberr) return next(dberr);
+    if(hashDirection){
+        console.log(hashDirection.dataValues.destination);
+        res.redirect(301,hashDirection.dataValues.destination);
+    }
+    else{
+        next();
+    }
+});
+router.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../../public/index.html'));
+});
 module.exports = router;
